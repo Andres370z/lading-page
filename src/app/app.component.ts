f174fcec1db6580dfdbb8fda506e3dde12fbef3e
Andres370z <rangelandres323@gmail.com>
2024-01-27 16:51:42 -0500

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
-
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { SafeUrl } from '@angular/platform-browser';
+import Swiper from 'swiper';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'loading-page';
+  public myAngularxQrCode: any;
+  public qrCodeDownloadLink: SafeUrl = "https://atlanticrebrokers.com/"
+  constructor() {
+    // assign a value
+    this.myAngularxQrCode = 'https://atlanticrebrokers.com/';
+  }
+  ngOnInit(): void {
+    const mySwiper = new Swiper('.swiper-container', {
+      // opciones de configuración de Swiper
+      slidesPerView: 5,
+      spaceBetween: 10,
+    })
+  }
+  onChangeURL(url: SafeUrl) {
+    this.qrCodeDownloadLink = url;
+  }
+  enviarWhatsapp(): void {
+    // Número de teléfono y mensaje
+    const phoneNumber = '+573106993585'; 
+    const message = 'Hola, ¿cómo estás, me gustaria mas información sobre tus productos?';
+
+    
+    const whatsappLink = `whatsapp://send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
+
+    
+    window.location.href = whatsappLink;
+  }
+  
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { QRCodeModule } from 'angularx-qrcode';
+import { NgImageSliderModule } from 'ng-image-slider';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    QRCodeModule,
+    NgImageSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
